a9d2a8210caf789a41bbda2bf18fe644dd897166
kyle <kaplaugher94@gmail.com>
2025-06-03 17:10:29 -0400

    config updates

--- lib/env.ts
+++ lib/env.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
-  NUXT_UI_PRO_LICENSE: z.string().optional(),
+  NUXT_UI_PRO_LICENSE: z.string(),
   TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string().optional()
+  TURSO_AUTH_TOKEN: z.string()
 })
 
 export type EnvSchema = z.infer<typeof EnvSchema>
