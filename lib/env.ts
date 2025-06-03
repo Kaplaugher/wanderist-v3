41dcdbd5f679cf3013eb2d55143356f7eb6ba232
kyle <kaplaugher94@gmail.com>
2025-06-03 17:14:28 -0400

    try optional for keys

--- lib/env.ts
+++ lib/env.ts
@@ -2,9 +2,9 @@ import { z } from 'zod'
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
-  NUXT_UI_PRO_LICENSE: z.string(),
-  TURSO_DATABASE_URL: z.string(),
-  TURSO_AUTH_TOKEN: z.string()
+  NUXT_UI_PRO_LICENSE: z.string().optional(),
+  TURSO_DATABASE_URL: z.string().optional(),
+  TURSO_AUTH_TOKEN: z.string().optional()
 })
 
 export type EnvSchema = z.infer<typeof EnvSchema>
